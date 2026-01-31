67eaf537c7769102fa23eb52027cbf892b70b2eb
Matias Labanda <matiacorazado33@gmail.com>
2026-01-31 17:40:37 -0500

    Add files via upload

---
+++ ProyectoDividido/postLogin.h
@@ -0,0 +1,11 @@
+#ifndef POSTLOGIN_H
+#define POSTLOGIN_H
+
+#include "preLogin.h"
+
+void estado_cuenta(Usuario *usuarios);
+void deposito(Usuario *usuarios);
+void retiro(Usuario *usuarios);
+void menu_inicio_sesion(Usuario *usuarios);
+
+#endif
---
+++ ProyectoDividido/preLogin.h
@@ -0,0 +1,21 @@
+#ifndef PRELOGIN_H
+#define PRELOGIN_H
+
+#include <stdio.h>
+#include <string.h>
+
+typedef struct { // Estructura para almacenar datos del usuario
+    char username[50]; // Nombre de usuario maximo 49 caracteres
+    char pin[5]; // PIN de 4 digitos + '\0'
+    float saldo; // Saldo en dolares
+    int numero_cuenta; // Numero de cuenta unico
+} Usuario;
+//Declarador de un arreglo global de usuarios
+extern Usuario usuarios[10000]; // Arreglo para almacenar hasta 10,000 usuarios
+extern int usercount; // Contador de usuarios registrados
+
+void registrar_usuario(Usuario usuarios[], int *usercount);
+void iniciarSesion(Usuario usuarios[], int usercount);
+void mostrarUsuarios(Usuario usuarios[], int usercount);
+
+#endif
